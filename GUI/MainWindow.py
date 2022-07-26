1697c5ebfb84a1d2ae1c1677341f0cdd76a671bd
Matt Lytle <mbl31@pitt.edu>
2022-07-26 19:49:45 -0400

    Creating GUI main window file

---
+++ GUI/MainWindow.py
@@ -0,0 +1,20 @@
+import os, sys
+
+p = os.path.abspath('.')
+sys.path.insert(1, p)
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import scrolledtext
+from tkinter.font import Font
+
+"""******************** Class for the main window of the GUI ********************"""
+class MainWindow():
+    pass
+"""******************** End MainWindow class ************************************"""
+
+
+
+
